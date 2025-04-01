160214b933e3d9d4cfffb98b92f05edf95d25a18
Peyton Gregory <peyton@peytongregory.com>
2025-04-01 19:07:05 -0400

    Home page CSS

--- index.php
+++ index.php
@@ -2,12 +2,14 @@
 <html>
 <head>
 <title>Employee Manager</title>
-<link rel="stylesheet" href="css/app.css">
+<link rel="stylesheet" href="/frontend/css/app.css">
 </head>
-<body>
+<body class="home login">
+    <div class="wrapper">
     <h1>Demo</h1>
     <ul>
-        <li><a href="/frontend/login.html">Login</a></li>
-        <li><a href="/frontend/employee_edit.html">Edit Profile</a></li>
+        <li><a class="btn login" href="/frontend/login.html">Login</a></li>
+        <li><a class="btn edit" href="/frontend/employee_edit.html">Edit Profile</a></li>
     </ul>
+    </div>
 </body>
